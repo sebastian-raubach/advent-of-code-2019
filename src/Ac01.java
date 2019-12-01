84ff06b9155dde8cd846461e60e4036e8c3a1edc
Sebastian Raubach <sebastian.raubach@hutton.ac.uk>
2019-12-01 18:09:48 +0000

    - CHG: Slight code optimization by only reading input once and reusing simple function in recursive function.

--- src/Ac01.java
+++ src/Ac01.java
@@ -2,25 +2,26 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Ac01 {
     public static void main(String[] args) throws IOException {
         Path input = new File("res/01.txt").toPath();
 
-        long sum = Files.readAllLines(input)
+        List<Integer> values = Files.readAllLines(input)
                 .stream()
                 .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        long sum = values.stream()
                 .mapToInt(Ac01::getFuelRequirement)
                 .sum();
-
         System.out.println(sum);
 
-        sum = Files.readAllLines(input)
-                .stream()
-                .map(Integer::parseInt)
+        sum = values.stream()
                 .mapToInt(Ac01::getFuelRequirementRecursive)
                 .sum();
-
         System.out.println(sum);
     }
 
@@ -29,7 +30,7 @@ public class Ac01 {
     }
 
     private static int getFuelRequirementRecursive(int input) {
-        int value = input / 3 - 2;
+        int value = getFuelRequirement(input);
 
         if (value > 0) {
             value += getFuelRequirementRecursive(value);
