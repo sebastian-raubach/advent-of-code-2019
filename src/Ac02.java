71fe2636681663027748c94c0d2a0e7d7658f0c5
Sebastian Raubach <sebastian.raubach@hutton.ac.uk>
2019-12-07 12:31:53 +0000

    - CHG: Refactored task 2 and 5 to use the new IntCode implementation.

--- src/Ac02.java
+++ src/Ac02.java
@@ -6,12 +6,28 @@ import java.util.Arrays;
 
 public class Ac02 {
     public static void main(String[] args) throws IOException {
-        System.out.println(computeOutput(12, 2));
+        Path input = new File("res/input/02.txt").toPath();
+        int[] inputValues = Arrays.stream(new String(Files.readAllBytes(input)).split(","))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int[] localValues = Arrays.copyOf(inputValues, inputValues.length);
+        localValues[1] = 12;
+        localValues[2] = 2;
+
+        IntCode first = new IntCode(localValues);
+        System.out.println(first.computeOutput());
 
         outer:
         for (int noun = 0; noun < 100; noun++) {
             for (int verb = 0; verb < 100; verb++) {
-                int output = computeOutput(noun, verb);
+                localValues = Arrays.copyOf(inputValues, inputValues.length);
+                localValues[1] = noun;
+                localValues[2] = verb;
+
+                IntCode intCode = new IntCode(localValues);
+
+                int output = intCode.computeOutput();
 
                 if (output == 19690720) {
                     System.out.println(String.format("%02d", noun) + String.format("%02d", verb));
@@ -19,40 +35,5 @@ public class Ac02 {
                 }
             }
         }
-    }
-
-    private static int computeOutput(int noun, int verb) throws IOException {
-        Path input = new File("res/input/02.txt").toPath();
-        Integer[] values = Arrays.stream(new String(Files.readAllBytes(input)).split(","))
-                .mapToInt(Integer::parseInt)
-                .boxed()
-                .toArray(Integer[]::new);
-
-        values[1] = noun;
-        values[2] = verb;
-
-        loop:
-        for (int i = 0; i < values.length; i += 4) {
-            Integer value = values[i];
-
-            int left = values[values[i + 1]];
-            int right = values[values[i + 2]];
-            int target = values[i + 3];
-
-            switch (value) {
-                case 99:
-                    break loop;
-                case 1:
-                    values[target] = left + right;
-                    break;
-                case 2:
-                    values[target] = left * right;
-                    break;
-                default:
-                    throw new IOException("Invalid opcode");
-            }
-        }
-
-        return values[0];
     }
 }
--- src/Ac05.java
+++ src/Ac05.java
@@ -1,6 +1,9 @@
-import java.io.*;
-import java.nio.file.*;
-import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * @author Sebastian Raubach
@@ -12,171 +15,17 @@ public class Ac05
 	public static void main(String[] args)
 		throws IOException
 	{
-		computeOutput();
-	}
-
-	private static int computeOutput()
-		throws IOException
-	{
 		Path input = new File("res/input/05.txt").toPath();
-		Integer[] values = Arrays.stream(new String(Files.readAllBytes(input)).split(","))
-								 .mapToInt(Integer::parseInt)
-								 .boxed()
-								 .toArray(Integer[]::new);
+        int[] inputValues = Arrays.stream(new String(Files.readAllBytes(input)).split(","))
+                .mapToInt(Integer::parseInt)
+                .toArray();
 
-		int i = 0;
-		loop:
-		while (i < values.length)
-		{
-			Integer value = values[i];
-			int increment = 0;
+        IntCode one = new IntCode(Arrays.copyOf(inputValues, inputValues.length));
+        while (one.canContinue())
+            System.out.println(one.computeOutput(1));
 
-			int[] instruction = instructionToParts(value);
-
-			int op = instruction[3] * 10 + instruction[4];
-			int delta;
-			switch (op)
-			{
-				case 99:
-					break loop;
-				case 1:
-					add(instruction, values, i);
-					increment = 4;
-					break;
-				case 2:
-					multiply(instruction, values, i);
-					increment = 4;
-					break;
-				case 3:
-					values[values[i + 1]] = in.nextInt();
-					increment = 2;
-					break;
-				case 4:
-					System.out.println(values[values[i + 1]]);
-					increment = 2;
-					break;
-				case 5:
-					delta = jumpIfTrue(instruction, values, i);
-					if (delta == 0)
-						increment = 3;
-					else
-						i = delta;
-					break;
-				case 6:
-					delta = jumpIfFalse(instruction, values, i);
-					if (delta == 0)
-						increment = 3;
-					else
-						i = delta;
-					break;
-				case 7:
-					lessThan(instruction, values, i);
-					increment = 4;
-					break;
-				case  8:
-					isEqual(instruction, values, i);
-					increment = 4;
-					break;
-			}
-
-			i += increment;
-		}
-
-		return values[0];
-	}
-
-	private static void lessThan(int[] instruction, Integer[] input, int index)
-	{
-		int first = instruction[2];
-		int second = instruction[1];
-
-		int left = first == 0 ? input[index + 1] : index + 1;
-		int right = second == 0 ? input[index + 2] : index + 2;
-		int target = input[index + 3];
-
-		if (input[left] < input[right])
-			input[target] = 1;
-		else
-			input[target] = 0;
-	}
-
-	private static void isEqual(int[] instruction, Integer[] input, int index)
-	{
-		int first = instruction[2];
-		int second = instruction[1];
-
-		int left = first == 0 ? input[index + 1] : index + 1;
-		int right = second == 0 ? input[index + 2] : index + 2;
-		int target = input[index + 3];
-
-		if (input[left].equals(input[right]))
-			input[target] = 1;
-		else
-			input[target] = 0;
-	}
-
-	private static int jumpIfTrue(int[] instruction, Integer[] input, int index)
-	{
-		int first = instruction[2];
-		int second = instruction[1];
-
-		int left = first == 0 ? input[index + 1] : index + 1;
-		int right = second == 0 ? input[index + 2] : index + 2;
-
-		if (input[left] != 0)
-			return input[right];
-		else
-			return 0;
-	}
-
-	private static int jumpIfFalse(int[] instruction, Integer[] input, int index)
-	{
-		int first = instruction[2];
-		int second = instruction[1];
-
-		int left = first == 0 ? input[index + 1] : index + 1;
-		int right = second == 0 ? input[index + 2] : index + 2;
-
-		if (input[left] == 0)
-			return input[right];
-		else
-			return 0;
-	}
-
-	private static void add(int[] instruction, Integer[] input, int index)
-	{
-		int first = instruction[2];
-		int second = instruction[1];
-
-		int left = first == 0 ? input[index + 1] : index + 1;
-		int right = second == 0 ? input[index + 2] : index + 2;
-		int target = input[index + 3];
-
-		input[target] = input[left] + input[right];
-	}
-
-	private static void multiply(int[] instruction, Integer[] input, int index)
-	{
-		int first = instruction[2];
-		int second = instruction[1];
-
-		int left = first == 0 ? input[index + 1] : index + 1;
-		int right = second == 0 ? input[index + 2] : index + 2;
-		int target = input[index + 3];
-
-		input[target] = input[left] * input[right];
-	}
-
-	private static int[] instructionToParts(Integer input)
-	{
-		int[] result = new int[5];
-		char[] parts = Integer.toString(input).toCharArray();
-
-		int delta = result.length - parts.length;
-
-		for (int i = result.length - 1; i >= delta; i--)
-			result[i] = Integer.parseInt(parts[i - delta] + "");
-
-		return result;
+        IntCode two = new IntCode(Arrays.copyOf(inputValues, inputValues.length));
+        while (two.canContinue())
+            System.out.println(two.computeOutput(5));
 	}
 }
